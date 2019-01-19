4bc5bfc86cbff346b33ceaf1af980d2ec15a4c10
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2019-01-19 11:45:36 +0000

    update to app.js related to populateItemList

--- js/app.js
+++ js/app.js
@@ -48,6 +48,9 @@ const ItemCtrl = (function () {
 
 // UI Controller
 const UICtrl = (function () {
+    const UISelectors = {
+        itemList: '#item-list'
+    }
 
     // Public methods
     return {
@@ -64,7 +67,7 @@ const UICtrl = (function () {
             });
 
             // Insert list items
-            document.querySelector('#item-list').innerHTML = html;
+            document.querySelector(UISelectors.itemList).innerHTML = html;
         }
 
     }
