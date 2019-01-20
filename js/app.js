5371b93c63833cf470a6d94c8db96086c1de3224
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2019-01-20 18:13:35 +0000

    Edit state parte one

--- js/app.js
+++ js/app.js
@@ -90,9 +90,12 @@ const UICtrl = (function () {
     const UISelectors = {
         itemList: '#item-list',
         addBtn: '.add-btn',
+        updateBtn: '.update-btn',
+        deleteBtn: '.delete-btn',
+        backBtn: '.back-btn',
         itemNameInput: '#item-name',
         itemCaloriesInput: '#item-calories',
-        totalCalories: '.total-calories'
+        totalCalories: '.total-calories',
     }
 
     // Public methods
@@ -151,6 +154,14 @@ const UICtrl = (function () {
         showTotalCalories: function (totalCalories) {
             document.querySelector(UISelectors.totalCalories).textContent = totalCalories;
         },
+        clearEditState: function () {
+            UICtrl.clearInput();
+
+            document.querySelector(UISelectors.updateBtn).style.display = 'none';
+            document.querySelector(UISelectors.deleteBtn).style.display = 'none';
+            document.querySelector(UISelectors.backBtn).style.display = 'none';
+            document.querySelector(UISelectors.addBtn).style.display = 'inline';
+        },
 
 
         //! ----------------- // RETURN // -----------------------------
@@ -174,6 +185,9 @@ const App = (function (ItemCtrl, UICtrl) {
 
         // Add Item event
         document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+
+        // Edit icon click event
+        document.querySelector(UISelectors.itemList).addEventListener('click', itemUpdateSubmit);
     }
 
 
@@ -208,10 +222,29 @@ const App = (function (ItemCtrl, UICtrl) {
         e.preventDefault();
     }
 
+    // Update item submit
+
+    const itemUpdateSubmit = function (e) {
+
+        // target icon
+        if (e.target.classList.contains('edit-item')) {
+            // Get list item id (item-0 , item-1)
+            // just get the id
+            const listId = e.target.parentNode.parentNode.id;
+
+            console.log(listId);
+
+
+        }
+
+        e.preventDefault();
+    }
+
     // Public methods
     return {
         init: function () {
-            // console.log('INItializing App...');
+            // Clear edit state / set initial state
+            UICtrl.clearEditState();
 
             // Fetch items from data structure
             const items = ItemCtrl.getItems();
